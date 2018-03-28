f896f17d255aaaefab442051d8ea22507dbeb1ba
Zane Suhadolnik <zanesuhi@gmail.com>
2018-03-28 06:42:28 +0200

    Added event handler for hero component

--- TourOfHeroes/angular-tour-of-heroes/src/app/heroes/heroes.component.ts
+++ TourOfHeroes/angular-tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -8,9 +8,13 @@ import { HEROES } from '../mock-heroes';
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
-  hero: Hero = {
+  selectedHero: Hero = {
     id: 1,
     name: 'Windstorm'
+  }
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
   }
 
   heroes = HEROES;
